d1ca5fef2d9e4aab256ace6c54868928025b986e
Marco Amador <amador.marco@gmail.com>
2020-06-20 18:33:49 +0100

    fix golangci-lint

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// main package for test application
 package main
 
 import (
@@ -12,13 +13,16 @@ import (
 	"github.com/Jeffail/benthos/v3/lib/types"
 )
 
+// HowSarcastic implements de sarcasm algorithm
 func HowSarcastic(content []byte) float64 {
 	if bytes.Contains(bytes.ToLower(content), []byte("/s")) {
-		return 100
+		const fullSarcastic = 100
+		return fullSarcastic
 	}
 	return 0
 }
 
+// SarcasmProc structure
 type SarcasmProc struct {
 	MetadataKey string `json:"metadata_key" yaml:"metadata_key"`
 }
@@ -26,7 +30,7 @@ type SarcasmProc struct {
 // ProcessMessage returns messages mutated with their sarcasm level.
 func (s *SarcasmProc) ProcessMessage(msg types.Message) ([]types.Message, types.Response) {
 	newMsg := msg.Copy()
-	newMsg.Iter(func(i int, p types.Part) error {
+	if err := newMsg.Iter(func(i int, p types.Part) error {
 		sarcasm := HowSarcastic(p.Get())
 		sarcasmStr := strconv.FormatFloat(sarcasm, 'f', -1, 64)
 		if len(s.MetadataKey) > 0 {
@@ -35,7 +39,9 @@ func (s *SarcasmProc) ProcessMessage(msg types.Message) ([]types.Message, types.
 			p.Set([]byte(sarcasmStr))
 		}
 		return nil
-	})
+	}); err != nil {
+		panic("Invalid config")
+	}
 	return []types.Message{newMsg}, nil
 }
 
